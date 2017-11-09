72a0f7e01247336185519eb81d474a6c58ee5c3b
Roman Malakhin <romanmalakhin@gmail.com>
2017-11-09 08:31:35 +0200

    Выполнил правки условий

--- index.php
+++ index.php
@@ -43,24 +43,23 @@ $lot_time_remaining = gmdate("H:i:s", $tomorrow - $now);
 
         <nav class="user-menu">
         
-            <?php if ($is_auth == true) { ?>
-                <div class="user-menu__image">
-                    <img src="<?php echo "$user_avatar"; ?>" width="40" height="40" alt="Пользователь">
-                </div>
-                <div class="user-menu__logged">
-                    <p><?php echo "$user_name"; ?></p>
-                </div>
-            <?php } else { ?>
-                <ul class="user-menu__list">
-                    <li class="user-menu__item">
-                        <a href="#">Регистрация</a>
-                    </li>
-                    <li class="user-menu__item">
-                        <a href="#">Вход</a>
-                    </li>
-                </ul>
-            <?php }  ?>
-
+            <?php if($is_auth): ?>
+                    <div class="user-menu__image">
+                        <img src="<?= $user_avatar; ?>" width="40" height="40" alt="Пользователь">
+                    </div>
+                    <div class="user-menu__logged">
+                        <p><?= $user_name; ?></p>
+                    </div>
+                <?php else: ?>
+                    <ul class="user-menu__list">
+                        <li class="user-menu__item">
+                            <a href="#">Регистрация</a>
+                        </li>
+                        <li class="user-menu__item">
+                            <a href="#">Вход</a>
+                        </li>
+                    </ul>
+                <?php endif; ?>
         </nav>
     </div>
 </header>
