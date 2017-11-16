9036675eeefd7004e49c77eb64c4bdabda1a1ebc
Roman Malakhin <romanmalakhin@gmail.com>
2017-11-16 07:54:28 +0200

    изменил имена переменных и циклы

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ $hours = floor(($remaining_seconds % 86400) / 3600);
 $lot_time_remaining = $hours . ":" . $minutes;
 
 // массивы
-$category = [
+$categoryList = [
     [
        'name' => 'Доски и лыжи',
        'img_prefix' => '--boards'
@@ -143,9 +143,9 @@ $products = [
         <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
 
         <ul class="promo__list">
-        <?php foreach ($category as $key => $val): ?>
-            <li class="promo__item promo__item<?=$val['img_prefix']; ?>">
-                <a class="promo__link" href="all-lots.html"><?=$val['name']; ?></a>
+        <?php foreach ($categoryList as $category): ?>
+            <li class="promo__item promo__item<?=$category['img_prefix']; ?>">
+                <a class="promo__link" href="all-lots.html"><?=$category['name']; ?></a>
             </li>
         <?php endforeach; ?>
         </ul>
@@ -155,18 +155,18 @@ $products = [
             <h2>Открытые лоты</h2>
         </div>
         <ul class="lots__list">
-        <?php foreach ($products as $key => $val): ?>
+        <?php foreach ($products as $product): ?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="<?=$val['url_img']; ?>" width="350" height="260" alt="<?=$val['category']; ?>">
+                    <img src="<?=$product['url_img']; ?>" width="350" height="260" alt="<?=$product['category']; ?>">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category"><?=$val['category']; ?></span>
-                    <h3 class="lot__title"><a class="text-link" href="lot.html"><?=$val['name']; ?></a></h3>
+                    <span class="lot__category"><?=$product['category']; ?></span>
+                    <h3 class="lot__title"><a class="text-link" href="lot.html"><?=$product['name']; ?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost"><?=$val['price']; ?><b class="rub">р</b></span>
+                            <span class="lot__cost"><?=$product['price']; ?><b class="rub">р</b></span>
                         </div>
                         <div class="lot__timer timer">
                             <?=$lot_time_remaining;?>
